05c65d77f35e0f7e471eb822d4745bb157d82915
ntaherni <ntaherni@ucsd.edu>
2021-12-21 15:21:53 -0800

    Organizing and refactoring

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Header } from './header';
+import Media from './media';
+import Pages from './tabs';
 import reportWebVitals from './reportWebVitals';
 
 function App() {
   return (
-    <Header />);
+    <div>
+      <Header />
+      <Media />
+      <Pages />
+    </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('#app'));
